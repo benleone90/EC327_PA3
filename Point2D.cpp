11bf5932eb20a732d741ef1448b4a4cf553e25f7
Ben Leone <benleone90@gmail.com>
2019-11-01 18:35:26 -0400

    Point2D.h updated with nonmember functions; added gitignore

--- Point2D.cpp
+++ Point2D.cpp
@@ -4,6 +4,5 @@ using namespace std;
 
 int main()
 {
-    double GetDistanceBetween(Point2D p1, Point2D p2);
-    return 0;
+    
 }
--- Point2D.h
+++ Point2D.h
@@ -1,5 +1,6 @@
 #ifndef Point2D_H
 #define Point2D_H
+#include <cmath>
 
 class Point2D
 {
@@ -18,5 +19,16 @@ class Point2D
         y = in_y;
     }
 };
+
+double GetDistanceBetween(Point2D p1, Point2D p2)
+{
+    double sqrt(pow((p2.x-p1.x),2)+pow((p2.y-p1.y),2));
+}
+
+ostream& operator <<(ostream& out, const Point2D& Point2D)
+{
+    out << (Point2D.x, Point2D.y) << endl;
+    return out;
+}
 
 #endif
